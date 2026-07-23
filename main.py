c48b93a8db3053409137f8234194a273df45497b
dharshinibalaji28-stack <dharshinibalaji28@gmail.com>
2026-07-23 21:57:39 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,275 +1,403 @@
 import tkinter as tk
-from tkinter import ttk, messagebox
+from tkinter import messagebox
+import random
+import string
 import csv
 import os
 from datetime import datetime
 import matplotlib.pyplot as plt
 
+# ---------------- Main Window ----------------
+
 root = tk.Tk()
-root.title("BMI Calculator Pro")
-root.geometry("650x700")
-root.configure(bg="#f5f7fa")
+root.title("Password Generator Pro")
+root.geometry("550x700")
+root.configure(bg="white")
 
-title = tk.Label(
+# ---------------- Heading ----------------
+
+heading = tk.Label(
     root,
-    text="BMI Calculator Pro",
-    font=("Arial",24,"bold"),
-    bg="#f5f7fa",
-    fg="#2c3e50"
+    text="Password Generator Pro",
+    font=("Arial", 24, "bold"),
+    bg="white",
+    fg="navy"
 )
-title.pack(pady=20)
+heading.pack(pady=20)
 
-frame = tk.Frame(root,bg="#ffffff",bd=2,relief="groove")
-frame.pack(padx=20,pady=10,fill="both")
+# ---------------- Password Length ----------------
 
-tk.Label(
-    frame,
-    text="Name",
-    font=("Arial",12,"bold"),
+length_label = tk.Label(
+    root,
+    text="Enter Password Length",
+    font=("Arial", 14),
     bg="white"
-).pack(pady=(20,5))
+)
+length_label.pack()
 
-name_entry=tk.Entry(frame,font=("Arial",12),width=30)
-name_entry.pack()
+length_entry = tk.Entry(
+    root,
+    font=("Arial", 14),
+    justify="center"
+)
+length_entry.pack(pady=10)
 
-tk.Label(
-    frame,
-    text="Weight (kg)",
-    font=("Arial",12,"bold"),
-    bg="white"
-).pack(pady=(15,5))
+# ---------------- Checkboxes ----------------
 
-weight_entry=tk.Entry(frame,font=("Arial",12),width=30)
-weight_entry.pack()
+upper_var = tk.BooleanVar(value=True)
+lower_var = tk.BooleanVar(value=True)
+number_var = tk.BooleanVar(value=True)
+symbol_var = tk.BooleanVar(value=True)
 
-tk.Label(
-    frame,
-    text="Height (m)",
-    font=("Arial",12,"bold"),
-    bg="white"
-).pack(pady=(15,5))
+tk.Checkbutton(
+    root,
+    text="Uppercase Letters",
+    variable=upper_var,
+    bg="white",
+    font=("Arial",12)
+).pack()
 
-height_entry=tk.Entry(frame,font=("Arial",12),width=30)
-height_entry.pack()
+tk.Checkbutton(
+    root,
+    text="Lowercase Letters",
+    variable=lower_var,
+    bg="white",
+    font=("Arial",12)
+).pack()
 
-result_label=tk.Label(
-    frame,
+tk.Checkbutton(
+    root,
+    text="Numbers",
+    variable=number_var,
+    bg="white",
+    font=("Arial",12)
+).pack()
+
+tk.Checkbutton(
+    root,
+    text="Symbols",
+    variable=symbol_var,
+    bg="white",
+    font=("Arial",12)
+).pack()
+
+# ---------------- Result Labels ----------------
+
+password_label = tk.Label(
+    root,
     text="",
-    font=("Arial",13,"bold"),
+    font=("Arial",18,"bold"),
+    bg="white",
+    fg="blue"
+)
+password_label.pack(pady=15)
+
+strength_label = tk.Label(
+    root,
+    text="",
+    font=("Arial",14,"bold"),
     bg="white"
 )
-result_label.pack(pady=20)
+strength_label.pack()
 
-filename="history.csv"
-def calculate_bmi():
+current_password = ""
+# ---------------- PASSWORD GENERATOR FUNCTION ---------------- #
+
+def generate_password():
+    characters = ""
+
+    if upper_var.get():
+        characters += string.ascii_uppercase
+
+    if lower_var.get():
+        characters += string.ascii_lowercase
+
+    if number_var.get():
+        characters += string.digits
+
+    if symbol_var.get():
+        characters += string.punctuation
+
+    if characters == "":
+        messagebox.showwarning("Warning", "Select at least one option!")
+        return
+
     try:
-        name = name_entry.get().strip()
+        length = int(length_entry.get())
+    except:
+        messagebox.showerror("Error", "Enter a valid password length")
+        return
 
-        if name == "":
-            messagebox.showerror("Error", "Please enter your name")
-            return
+    password = "".join(random.choice(characters) for _ in range(length))
 
-        weight = float(weight_entry.get())
-        height = float(height_entry.get())
+    password_label.config(text=password)
 
-        if weight <= 0 or height <= 0:
-            messagebox.showerror("Error", "Weight and Height must be greater than 0")
-            return
+    strength = check_strength(password)
+    strength_label.config(text=f"Password Strength: {strength}")
 
-        bmi = weight / (height * height)
+    save_history(password, strength)
 
-        if bmi < 18.5:
-            category = "Underweight"
-            color = "blue"
-        elif bmi < 25:
-            category = "Normal"
-            color = "green"
-        elif bmi < 30:
-            category = "Overweight"
-            color = "orange"
-        else:
-            category = "Obese"
-            color = "red"
 
-        result_label.config(
-            text=f"Name : {name}\n\nBMI : {round(bmi,2)}\n\nCategory : {category}",
-            fg=color
-        )
+# ---------------- PASSWORD STRENGTH ---------------- #
 
-        file_exists = os.path.isfile(filename)
+def check_strength(password):
+    score = 0
 
-        with open(filename, "a", newline="") as file:
-            writer = csv.writer(file)
+    if len(password) >= 8:
+        score += 1
 
-            if not file_exists:
-                writer.writerow([
-                    "Date",
-                    "Name",
-                    "Weight",
-                    "Height",
-                    "BMI",
-                    "Category"
-                ])
+    if any(c.isupper() for c in password):
+        score += 1
 
+    if any(c.islower() for c in password):
+        score += 1
+
+    if any(c.isdigit() for c in password):
+        score += 1
+
+    if any(c in string.punctuation for c in password):
+        score += 1
+
+    if score <= 2:
+        return "Weak"
+    elif score <= 4:
+        return "Medium"
+    else:
+        return "Strong"
+    # ---------------- SAVE HISTORY ---------------- #
+
+def save_history(password, strength):
+
+    file_exists = os.path.isfile("history.csv")
+
+    with open("history.csv", "a", newline="") as file:
+
+        writer = csv.writer(file)
+
+        if not file_exists:
             writer.writerow([
-                datetime.now().strftime("%d-%m-%Y %H:%M"),
-                name,
-                weight,
-                height,
-                round(bmi,2),
-                category
+                "Date",
+                "Password",
+                "Strength"
             ])
 
-    except ValueError:
-        messagebox.showerror(
-            "Error",
-            "Please enter valid numeric values."
+        writer.writerow([
+            datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
+            password,
+            strength
+        ])
+
+
+# ---------------- COPY PASSWORD ---------------- #
+
+def copy_password():
+
+    password = password_label.cget("text")
+
+    if password == "":
+        messagebox.showwarning(
+            "Warning",
+            "Generate a password first!"
         )
-def show_history():
-    if not os.path.exists(filename):
-        messagebox.showinfo("History", "No history found.")
+        return
+
+    root.clipboard_clear()
+    root.clipboard_append(password)
+    root.update()
+
+    messagebox.showinfo(
+        "Success",
+        "Password copied to clipboard!"
+    )
+
+
+# ---------------- CLEAR ---------------- #
+
+def clear_all():
+
+    length_entry.delete(0, tk.END)
+
+    upper_var.set(True)
+    lower_var.set(True)
+    number_var.set(True)
+    symbol_var.set(True)
+
+    password_label.config(text="")
+    strength_label.config(text="")
+    # ---------------- VIEW HISTORY ---------------- #
+
+def view_history():
+
+    if not os.path.exists("history.csv"):
+        messagebox.showinfo("History", "No history found!")
         return
 
     history_window = tk.Toplevel(root)
-    history_window.title("BMI History")
-    history_window.geometry("700x400")
+    history_window.title("Password History")
+    history_window.geometry("600x400")
 
-    tree = ttk.Treeview(
-        history_window,
-        columns=("Date", "Name", "Weight", "Height", "BMI", "Category"),
-        show="headings"
-    )
+    text = tk.Text(history_window, font=("Consolas", 10))
+    text.pack(fill="both", expand=True)
 
-    tree.heading("Date", text="Date")
-    tree.heading("Name", text="Name")
-    tree.heading("Weight", text="Weight")
-    tree.heading("Height", text="Height")
-    tree.heading("BMI", text="BMI")
-    tree.heading("Category", text="Category")
+    with open("history.csv", "r") as file:
+        text.insert(tk.END, file.read())
 
-    tree.column("Date", width=140)
-    tree.column("Name", width=100)
-    tree.column("Weight", width=80)
-    tree.column("Height", width=80)
-    tree.column("BMI", width=80)
-    tree.column("Category", width=120)
 
-    tree.pack(fill="both", expand=True)
+# ---------------- BAR GRAPH ---------------- #
 
-    with open(filename, "r") as file:
-        reader = csv.reader(file)
-        next(reader)
+def show_bar_graph():
 
-        for row in reader:
-            tree.insert("", tk.END, values=row)
-def show_graph():
-
-    if not os.path.exists(filename):
-        messagebox.showinfo("Graph", "No data available.")
+    if not os.path.exists("history.csv"):
+        messagebox.showinfo("Graph", "No history found!")
         return
 
-    names = []
-    bmi_values = []
+    weak = 0
+    medium = 0
+    strong = 0
 
-    with open(filename, "r") as file:
+    with open("history.csv", "r") as file:
+
         reader = csv.DictReader(file)
 
         for row in reader:
-            names.append(row["Name"])
-            bmi_values.append(float(row["BMI"]))
 
-    plt.figure(figsize=(8,5))
-    plt.bar(names, bmi_values)
-    plt.title("BMI History")
-    plt.xlabel("Users")
-    plt.ylabel("BMI")
-    plt.grid(axis="y")
+            if row["Strength"] == "Weak":
+                weak += 1
+
+            elif row["Strength"] == "Medium":
+                medium += 1
+
+            elif row["Strength"] == "Strong":
+                strong += 1
+
+    plt.figure(figsize=(5,4))
+    plt.bar(
+        ["Weak", "Medium", "Strong"],
+        [weak, medium, strong],
+        color=["red", "orange", "green"]
+    )
+
+    plt.title("Password Strength Distribution")
+    plt.xlabel("Strength")
+    plt.ylabel("Count")
     plt.show()
 
+
+# ---------------- PIE CHART ---------------- #
 
 def show_pie_chart():
 
-    if not os.path.exists(filename):
-        messagebox.showinfo("Chart", "No data available.")
+    if not os.path.exists("history.csv"):
+        messagebox.showinfo("Graph", "No history found!")
         return
 
-    categories = {
-        "Underweight":0,
-        "Normal":0,
-        "Overweight":0,
-        "Obese":0
-    }
+    weak = 0
+    medium = 0
+    strong = 0
 
-    with open(filename, "r") as file:
+    with open("history.csv", "r") as file:
+
         reader = csv.DictReader(file)
 
         for row in reader:
-            categories[row["Category"]] += 1
 
-    plt.figure(figsize=(6,6))
+            if row["Strength"] == "Weak":
+                weak += 1
+
+            elif row["Strength"] == "Medium":
+                medium += 1
+
+            elif row["Strength"] == "Strong":
+                strong += 1
+
+    plt.figure(figsize=(5,5))
     plt.pie(
-        categories.values(),
-        labels=categories.keys(),
-        autopct="%1.1f%%",
-        startangle=90
+        [weak, medium, strong],
+        labels=["Weak", "Medium", "Strong"],
+        autopct="%1.1f%%"
     )
 
-    plt.title("BMI Category Distribution")
+    plt.title("Password Strength Distribution")
     plt.show()
-button_frame = tk.Frame(root, bg="#f5f7fa")
-button_frame.pack(pady=20)
+    # ---------------- BUTTONS ---------------- #
 
-calculate_button = tk.Button(
-    button_frame,
-    text="Calculate BMI",
-    command=calculate_bmi,
-    font=("Arial",12,"bold"),
-    bg="#4CAF50",
+generate_button = tk.Button(
+    root,
+    text="Generate Password",
+    command=generate_password,
+    bg="green",
     fg="white",
-    width=18
+    font=("Arial",12,"bold"),
+    width=22
 )
-calculate_button.grid(row=0, column=0, padx=10, pady=10)
+generate_button.pack(pady=5)
+
+copy_button = tk.Button(
+    root,
+    text="Copy Password",
+    command=copy_password,
+    bg="blue",
+    fg="white",
+    font=("Arial",12,"bold"),
+    width=22
+)
+copy_button.pack(pady=5)
 
 history_button = tk.Button(
-    button_frame,
+    root,
     text="View History",
-    command=show_history,
-    font=("Arial",12,"bold"),
-    bg="#2196F3",
+    command=view_history,
+    bg="orange",
     fg="white",
-    width=18
+    font=("Arial",12,"bold"),
+    width=22
 )
-history_button.grid(row=0, column=1, padx=10, pady=10)
+history_button.pack(pady=5)
 
-graph_button = tk.Button(
-    button_frame,
-    text="Bar Graph",
-    command=show_graph,
-    font=("Arial",12,"bold"),
-    bg="#FF9800",
+bar_button = tk.Button(
+    root,
+    text="Show Bar Graph",
+    command=show_bar_graph,
+    bg="purple",
     fg="white",
-    width=18
+    font=("Arial",12,"bold"),
+    width=22
 )
-graph_button.grid(row=1, column=0, padx=10, pady=10)
+bar_button.pack(pady=5)
 
 pie_button = tk.Button(
-    button_frame,
-    text="Pie Chart",
+    root,
+    text="Show Pie Chart",
     command=show_pie_chart,
-    font=("Arial",12,"bold"),
-    bg="#9C27B0",
+    bg="brown",
     fg="white",
-    width=18
+    font=("Arial",12,"bold"),
+    width=22
 )
-pie_button.grid(row=1, column=1, padx=10, pady=10)
+pie_button.pack(pady=5)
+
+clear_button = tk.Button(
+    root,
+    text="Clear",
+    command=clear_all,
+    bg="red",
+    fg="white",
+    font=("Arial",12,"bold"),
+    width=22
+)
+clear_button.pack(pady=5)
+
+# ---------------- FOOTER ---------------- #
 
 footer = tk.Label(
     root,
-    text="Developed using Python, Tkinter, CSV and Matplotlib",
+    text="Developed by B. Dharshini\nOASIS INFOBYTE - Python Programming",
     font=("Arial",10),
-    bg="#f5f7fa",
+    bg="white",
     fg="gray"
 )
-footer.pack(pady=15)
+footer.pack(pady=20)
+
+# ---------------- START PROGRAM ---------------- #
 
 root.mainloop()
